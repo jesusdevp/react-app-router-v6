5a7a5341925825e498e5e895e34966c037162ed0
Jesus Ramirez <jesusdevelp@gmail.com>
2023-06-28 08:35:14 -0600

    Lazyload basic

---
+++ src/routes/routes.ts
@@ -0,0 +1,35 @@
+import { LazyExoticComponent, lazy } from "react";
+
+type JSXComponent = () => JSX.Element
+
+interface Route {
+    to: string,
+    path: string,
+    name: string,
+    Component: LazyExoticComponent<JSXComponent> | JSXComponent
+}
+
+const lazy1 = lazy(() => import('../01-lazyload/pages/LazyPage1'))
+const lazy2 = lazy(() => import('../01-lazyload/pages/LazyPage2'))
+const lazy3 = lazy(() => import('../01-lazyload/pages/LazyPage3'))
+
+export const routes : Route[] = [
+    {
+        to: '/lazy1',
+        path: 'lazy1',
+        Component: lazy1,
+        name: 'Lazy-1'
+    },
+    {
+        to: '/lazy2',
+        path: 'lazy2',
+        Component: lazy2,
+        name: 'Lazy-2'
+    },
+    {
+        to: '/lazy3',
+        path: 'lazy3',
+        Component: lazy3,
+        name: 'Lazy-3'
+    }
+]
